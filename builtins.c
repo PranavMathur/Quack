eed60d3cde9d72efd409ef65cea828ad3d272253
Pranav Mathur <pranavmathur99@gmail.com>
2022-02-07 12:58:22 -0800

    Implement String comparison methods

--- builtins.c
+++ builtins.c
@@ -315,6 +315,114 @@ vm_Word method_String_equals[] = {
 };
 
 
+obj_ref native_String_less(void ) {
+    obj_ref this = vm_fp->obj;
+    assert_is_type(this, the_class_String);
+    obj_String this_str = (obj_String) this;
+    obj_ref other = (vm_fp - 1)->obj;
+    assert_is_type(other, the_class_String);
+    obj_String other_str = (obj_String) other;
+    if (strcmp(this_str->text, other_str->text) < 0) {
+        return lit_true;
+    } else {
+        return lit_false;
+    }
+}
+
+vm_Word method_String_less[] = {
+        {.instr = vm_op_enter},
+        {.instr = vm_op_load},
+        {.intval = 0},   // this
+        {.instr = vm_op_load},
+        {.intval = -1},  // other
+        {.instr = vm_op_call_native},
+        {.native = native_String_less},
+        {.instr = vm_op_return},
+        {.intval = 1}  // consume other
+};
+
+
+obj_ref native_String_atmost(void ) {
+    obj_ref this = vm_fp->obj;
+    assert_is_type(this, the_class_String);
+    obj_String this_str = (obj_String) this;
+    obj_ref other = (vm_fp - 1)->obj;
+    assert_is_type(other, the_class_String);
+    obj_String other_str = (obj_String) other;
+    if (strcmp(this_str->text, other_str->text) <= 0) {
+        return lit_true;
+    } else {
+        return lit_false;
+    }
+}
+
+vm_Word method_String_atmost[] = {
+        {.instr = vm_op_enter},
+        {.instr = vm_op_load},
+        {.intval = 0},   // this
+        {.instr = vm_op_load},
+        {.intval = -1},  // other
+        {.instr = vm_op_call_native},
+        {.native = native_String_atmost},
+        {.instr = vm_op_return},
+        {.intval = 1}  // consume other
+};
+
+
+obj_ref native_String_more(void ) {
+    obj_ref this = vm_fp->obj;
+    assert_is_type(this, the_class_String);
+    obj_String this_str = (obj_String) this;
+    obj_ref other = (vm_fp - 1)->obj;
+    assert_is_type(other, the_class_String);
+    obj_String other_str = (obj_String) other;
+    if (strcmp(this_str->text, other_str->text) > 0) {
+        return lit_true;
+    } else {
+        return lit_false;
+    }
+}
+
+vm_Word method_String_more[] = {
+        {.instr = vm_op_enter},
+        {.instr = vm_op_load},
+        {.intval = 0},   // this
+        {.instr = vm_op_load},
+        {.intval = -1},  // other
+        {.instr = vm_op_call_native},
+        {.native = native_String_more},
+        {.instr = vm_op_return},
+        {.intval = 1}  // consume other
+};
+
+
+obj_ref native_String_atleast(void ) {
+    obj_ref this = vm_fp->obj;
+    assert_is_type(this, the_class_String);
+    obj_String this_str = (obj_String) this;
+    obj_ref other = (vm_fp - 1)->obj;
+    assert_is_type(other, the_class_String);
+    obj_String other_str = (obj_String) other;
+    if (strcmp(this_str->text, other_str->text) >= 0) {
+        return lit_true;
+    } else {
+        return lit_false;
+    }
+}
+
+vm_Word method_String_atleast[] = {
+        {.instr = vm_op_enter},
+        {.instr = vm_op_load},
+        {.intval = 0},   // this
+        {.instr = vm_op_load},
+        {.intval = -1},  // other
+        {.instr = vm_op_call_native},
+        {.native = native_String_atleast},
+        {.instr = vm_op_return},
+        {.intval = 1}  // consume other
+};
+
+
 /* String:PLUS  */
 obj_ref native_String_plus(void ) {
     obj_ref this = vm_fp->obj;
@@ -360,7 +468,10 @@ struct  class_struct  the_class_String_struct = {
         method_String_string,
         method_String_print,
         method_String_equals,
-        method_tbd_1, //method_String_less,
+        method_String_less,
+        method_String_atmost,
+        method_String_more,
+        method_String_atleast,
         method_String_plus
 };
 
