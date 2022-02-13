1e3430ad3f315d9d5095f6d82b96646a2882d78a
Pranav Mathur <pranavmathur99@gmail.com>
2022-02-13 14:51:18 -0800

    Set type attribute of tree in transformer

--- compiler/transformer.py
+++ compiler/transformer.py
@@ -24,58 +24,69 @@ assign_ops = (
     'mod_equals'
 )
 
+#helper method to create a new tree with a "type" attribute
+def typed_tree(*args):
+    ret = Tree(*args)
+    ret.type = ''
+    return ret
+
 #operates on the tree as it is created
 #desugars binary operators into method calls
 @lark.v_args(tree=True)
 class Transformer(lark.Transformer):
+    #transform the tree, then set its "type" attribute
+    def _transform_tree(self, tree):
+        ret = super()._transform_tree(tree)
+        ret.type = ''
+        return ret
     #"!=" is translated into "==" followed by a negation
     def notequals(self, tree):
         #children of the intermediate call's node
         eq_children = [
             tree.children[0], #receiver of "!=" (LHS)
             'EQUALS',
-            Tree('m_args', tree.children[1:]) #argument (RHS)
+            typed_tree('m_args', tree.children[1:]) #argument (RHS)
         ]
         #intermediate call's node
-        eq_call = Tree('m_call', eq_children)
+        eq_call = typed_tree('m_call', eq_children)
         #children of the returned node
         new_children = [
             eq_call, #receiver of negation
             'NEGATE',
-            Tree('m_args', []) #boolean negation has no arguments
+            typed_tree('m_args', []) #boolean negation has no arguments
         ]
-        return Tree('m_call', new_children)
+        return typed_tree('m_call', new_children)
     #create a method call subtree with the appropriate binary op function
     def op_transform(self, data, children, meta):
         #desugar binary operations into method calls
         new_children = [
             children[0], #receiver object
             data.upper(), #name of operator
-            Tree('m_args', children[1:]) #argument object, if provided
+            typed_tree('m_args', children[1:]) #argument object, if provided
         ]
-        return Tree('m_call', new_children)
+        return typed_tree('m_call', new_children)
     #create an assignment subtree that assigns to the result of a method call
     def assign_op(self, data, children, meta):
         method = data[:-7].upper() #extract the appropriate binary operator
         left, right = children #unpack the arguments to the operator
-        var_node = Tree('var', [left]) #create the variable node for the LHS
+        var_node = typed_tree('var', [left]) #create the variable node for the LHS
         method_children = [
             var_node, #receiver object
             method, #name of binary op's associated method
-            Tree('m_args', [right]) #argument subtree
+            typed_tree('m_args', [right]) #argument subtree
         ]
         #create the method call subtree
-        method_node = Tree('m_call', method_children)
+        method_node = typed_tree('m_call', method_children)
         assign_children = [
             left, #LHS of assignment
             method_node #RHS of assignment
         ]
         #create the assignment subtree
-        return Tree('assign_imp', assign_children)
+        return typed_tree('assign_imp', assign_children)
     def __default__(self, data, children, meta):
         if data in ops: #only desugar certain nodes
             return self.op_transform(data, children, meta)
         elif data in assign_ops:
             return self.assign_op(data, children, meta)
         else:
-            return Tree(data, children, meta)
+            return typed_tree(data, children, meta)
