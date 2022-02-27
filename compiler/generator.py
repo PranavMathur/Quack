6010ae3d613640881d9aa1341a68a7788cfcd96b
Pranav Mathur <pranavmathur99@gmail.com>
2022-02-26 21:50:38 -0800

    PEP8-ify generator

--- compiler/generator.py
+++ compiler/generator.py
@@ -12,6 +12,7 @@ preorder = (
     'store_field'
 )
 
+
 #generate assembly code from the parse tree
 class Generator(lark.visitors.Visitor_Recursive):
     def __init__(self, classes, types):
@@ -25,18 +26,21 @@ class Generator(lark.visitors.Visitor_Recursive):
         self.current_method = None
         #method table of builtin and user-defined classes
         self.types = types
-        #label prefix counter
-        self.labels = dd(itertools.count) #stores count of label prefixes
+        #stores count of label prefixes
+        self.labels = dd(itertools.count)
+
     def emit(self, line, tab=True):
         #emits a line of code to the output array
         #adds a tab to the beginning by default
         if tab:
             line = '\t' + line
         self.current_method['code'].append(line)
+
     def label(self, prefix):
         #generates a unique label name with the given prefix
         num = next(self.labels[prefix]) #get current number for given prefix
         return f'{prefix}_{num}'
+
     def visit(self, tree):
         #some nodes need to be visited before their children
         #if this node is such a node, visit it directly
@@ -46,21 +50,25 @@ class Generator(lark.visitors.Visitor_Recursive):
         else:
             #most expressions are traversed postorder
             return super().visit(tree)
+
     def class_(self, tree):
         #extract class's name and supertype
         name = str(tree.children[0].children[0])
         #if no supertype is given, default to 'Obj'
         sup = str(tree.children[0].children[2] or 'Obj')
         #create class object
+
         obj = {
             'name': name,
             'super': sup,
             'methods': [],
             'fields': {}
         }
+
         self.current_class = obj
         #store class object in result array
         self.classes_.append(obj)
+
         #attempt to retrieve the fields of this class from the method table
         try:
             type_obj = self.types[name]
@@ -70,13 +78,16 @@ class Generator(lark.visitors.Visitor_Recursive):
         else:
             #populate class object with fields from method table
             obj['fields'] = set(type_obj['fields'])
+
         #generate code for all methods in the class
         for method in tree.children[1].children[0].children:
             self.visit(method)
+
     def method(self, tree):
         #extract class's name and formal arguments
         name = str(tree.children[0])
         args = tree.children[1]
+
         #create method object
         obj = {
             'name': name,
@@ -84,34 +95,44 @@ class Generator(lark.visitors.Visitor_Recursive):
             'locals': {}, #stores names and types of local variables
             'code': [] #stores assembly code for the method
         }
+
         #add method object to the current class
         self.current_class['methods'].append(obj)
         #store the current method for use in other generator functions
         self.current_method = obj
+
         #all methods start with an enter command
         self.emit('enter')
+
         #iterate over statements in the method's statement block
         for child in tree.children[3].children:
             self.visit(child)
+
         #push nothing as return value - TODO: change this
         self.emit('const nothing')
         #return from the method, popping off arguments
         self.emit('return %s' % len(obj['args']))
+
     def lit_number(self, tree):
         #push an integer onto the stack
         self.emit('const %s' % tree.children[0])
+
     def lit_true(self, tree):
         #push a boolean onto the stack
         self.emit('const true')
+
     def lit_false(self, tree):
         #push a boolean onto the stack
         self.emit('const false')
+
     def lit_nothing(self, tree):
         #push a nothing onto the stack
         self.emit('const nothing')
+
     def lit_string(self, tree):
         #push a string onto the stack
         self.emit('const %s' % tree.children[0])
+
     def var(self, tree):
         #extract variable name from tree
         v_name = str(tree.children[0])
@@ -122,11 +143,13 @@ class Generator(lark.visitors.Visitor_Recursive):
         else:
             #load a local variable onto the stack
             self.emit('load %s' % tree.children[0])
+
     def load_field(self, tree):
         #unpack children for convenience
         obj, field = tree.children
         #load the given variable onto the stack
         self.emit('load_field %s:%s' % (obj.type, field))
+
     def assign(self, tree):
         #store the top value on the stack into a local variable
         name = tree.children[0]
@@ -138,6 +161,7 @@ class Generator(lark.visitors.Visitor_Recursive):
         self.current_method['locals'][name] = type
         #emit a store instruction
         self.emit('store %s' % name)
+
     def store_field(self, tree):
         #unpack children for convenience
         obj, field, value = tree.children
@@ -151,6 +175,7 @@ class Generator(lark.visitors.Visitor_Recursive):
         #pop two values of the stack, then store the value of the second pop
         #in the object from the first pop in the provided field
         self.emit('store_field %s:%s' % (c_name, field))
+
     def m_call(self, tree):
         #emit a method call command and possibly a roll
         m_name = str(tree.children[1])
@@ -162,64 +187,79 @@ class Generator(lark.visitors.Visitor_Recursive):
         left_type = tree.children[0].type
         #emit a method call of the correct type
         self.emit('call %s:%s' % (left_type, tree.children[1]))
+
     def c_call(self, tree):
         c_name = str(tree.children[0])
         #allocate space for a new object of type c_name
         self.emit('new %s' % c_name)
         #call the constructor on the new object
         self.emit('call %s:$constructor' % c_name)
+
     def raw_rexp(self, tree):
         #if a statement is just a right_expression, the value of the expression
         #stays on the stack but is not used, so it can be popped
         self.emit('pop')
+
     def and_exp(self, tree):
         left, right = tree.children
-        #generate assembly for first expression, which will always run
-        self.visit(left)
         #generate unique label names
         false_label = self.label('and')
         join_label = self.label('and')
+
+        #generate assembly for first expression, which will always run
+        self.visit(left)
         #if the first expression evaluates to false, jump to join point
         self.emit('jump_ifnot %s' % false_label)
+
         #generate assembly for second expression
         #this will only run if the first expression evaluated to true
         self.visit(right)
         #if the second expression evaluates to false, jump to join point
         self.emit('jump_ifnot %s' % false_label)
+
         #if neither jump was taken, push true as the result
         self.emit('const true')
         #skip past the join point
         self.emit('jump %s' % join_label)
+
         #join point: execution will come here if either expression is false
         self.emit('%s:' % false_label, False)
         #if either jump was taken, push false as the result
         self.emit('const false')
+
         #and expression is over - join point
         self.emit('%s:' % join_label, False)
+
     def or_exp(self, tree):
         left, right = tree.children
-        #generate assembly for first expression, which will always run
-        self.visit(left)
         #generate unique label names
         true_label = self.label('or')
         join_label = self.label('or')
+
+        #generate assembly for first expression, which will always run
+        self.visit(left)
         #if the first expression evaluates to true, jump to join point
         self.emit('jump_if %s' % true_label)
+
         #generate assembly for second expression
         #this will only run if the first expression evaluated to false
         self.visit(right)
         #if the second expression evaluates to true, jump to join point
         self.emit('jump_if %s' % true_label)
+
         #if neither jump was taken, push false as the result
         self.emit('const false')
         #skip past the join point
         self.emit('jump %s' % join_label)
+
         #join point: execution will come here if either expression is true
         self.emit('%s:' % true_label, False)
         #if either jump was taken, push true as the result
         self.emit('const true')
+
         #or expression is over - join point
         self.emit('%s:' % join_label, False)
+
     def if_stmt(self, tree):
         #unpack children nodes for convenience
         if_cond, if_block, elifs, _else = tree.children
@@ -284,24 +324,29 @@ class Generator(lark.visitors.Visitor_Recursive):
 
         #emit the join label - this point will always be reached
         self.emit('%s:' % join_label, False)
+
     def while_lp(self, tree):
         #unpack children nodes for convenience
         condition, block = tree.children
         #generate unique labels for block and condition
         block_label = self.label('while_block')
         cond_label = self.label('while_cond')
+
         #unconditionally jump to condition check
         self.emit('jump %s' % cond_label)
         #emit label for start of block
         self.emit('%s:' % block_label, False)
+
         #generate code for block
         self.visit(block)
         #emit label for condition check
         self.emit('%s:' % cond_label, False)
+
         #generate code for condition check
         self.visit(condition)
         #if condition evaluates to true, jump to beginning of block
         self.emit('jump_if %s' % block_label)
+
 
 #generates assembly file for the given class object
 def generate_file(class_):
@@ -310,6 +355,7 @@ def generate_file(class_):
     sup = class_['super']
     methods = class_['methods']
     fields = class_['fields']
+
     #data will be output to file with the same name as the class
     filename = name + '.asm'
     #open the output file for writing
@@ -319,6 +365,7 @@ def generate_file(class_):
         #if there are any fields, output their names
         for field in fields:
             f.write('.field %s\n' % field)
+
         #for each method, output a forward declaration
         for method in methods:
             m_name = method['name']
@@ -326,6 +373,7 @@ def generate_file(class_):
             if m_name != '$constructor':
                 f.write('.method %s forward\n' % m_name)
         f.write('\n')
+
         #for each method, output assembly for the method
         for method in methods:
             #extract data from method object
@@ -333,6 +381,7 @@ def generate_file(class_):
             args = method['args']
             locals = method['locals']
             code = method['code']
+
             #output method header
             f.write('.method %s\n' % m_name)
             #if the method takes arguments, output their names
@@ -343,6 +392,7 @@ def generate_file(class_):
             if locals:
                 s = ','.join(locals)
                 f.write('.local %s\n' % s)
+
             #output assembly for each instruction in the method
             for line in code:
                 f.write(line + '\n')
