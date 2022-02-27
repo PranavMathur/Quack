fdb9cd167d7067851725f578af5c9f34e6380a56
Pranav Mathur <pranavmathur99@gmail.com>
2022-02-26 22:40:41 -0800

    PEP8-ify transformer

--- compiler/transformer.py
+++ compiler/transformer.py
@@ -25,6 +25,7 @@ assign_ops = (
     'mod_equals'
 )
 
+
 #operates on the tree as it is created
 #desugars binary operators into method calls
 @lark.v_args(tree=True)
@@ -41,6 +42,7 @@ class OpTransformer(lark.Transformer):
             'NEGATE',
             Tree('args', []) #NEGATE takes no arguments
         ])
+
     def store_field(self, tree):
         #rearrange a store_field node to look more like an assignment
         #the grammar causes store_field to have a load_field child,
@@ -53,6 +55,7 @@ class OpTransformer(lark.Transformer):
             typ = 'method' if load.data == 'm_call' else 'constructor'
             e = 'Cannot assign to a %s call' % typ
             raise CompileError(e)
+
         #unpack children of load_field for convenience
         obj, field = load.children
         #construct return store_field node
@@ -62,6 +65,7 @@ class OpTransformer(lark.Transformer):
             field,
             value
         ])
+
     #create a method call subtree with the appropriate binary op function
     def op_transform(self, data, children, meta):
         #desugar binary operations into method calls
@@ -70,6 +74,7 @@ class OpTransformer(lark.Transformer):
             data.upper(), #name of operator
             Tree('args', children[1:]) #argument object, if provided
         ])
+
     #create an assignment subtree that assigns to the result of a method call
     def assign_op(self, data, children, meta):
         method = data[:-7].upper() #extract the appropriate binary operator
@@ -79,6 +84,7 @@ class OpTransformer(lark.Transformer):
             #handle assignment operators on fields
             #unpack object and field name for convenience
             obj, field = children[0].children
+
             #create the assignment subtree
             return Tree('store_field', [
                 Tree('load_field', [
@@ -94,10 +100,12 @@ class OpTransformer(lark.Transformer):
                     Tree('args', [right]) #argument subtree
                 ])
             ])
+
         else:
             if isinstance(left, Tree):
                 left = left.children[0]
             #create the assignment subtree
+
             return Tree('assign', [
                 left, #LHS of assignment
                 None, #let the type checker imply the type
@@ -107,6 +115,7 @@ class OpTransformer(lark.Transformer):
                     Tree('args', [right]) #argument subtree
                 ])
             ])
+
     def __default__(self, data, children, meta):
         if data in ops: #only desugar certain nodes
             return self.op_transform(data, children, meta)
@@ -115,16 +124,19 @@ class OpTransformer(lark.Transformer):
         else:
             return Tree(data, children, meta)
 
+
 #creates Main class from code at the end of the file
 @lark.v_args(tree=True)
 class ClassTransformer(lark.Transformer):
     def __init__(self, name):
         self.name = name
+
     #process code at the end of the file
     def main_block(self, tree):
         #do nothing if there is no code to be executed
         if not tree.children:
             return tree
+
         #create and return a subtree for a new main class
         #the new class will have only one method, the constructor
         return Tree('class_', [
@@ -152,14 +164,17 @@ class ClassTransformer(lark.Transformer):
                 ])
             ])
         ])
+
     #move the main class created above into the correct subtree
     def program(self, tree):
         classes = tree.children[0]
         #remove main class subtree from tree
         main_class = tree.children.pop(1)
+
         #do nothing if no main class was created
         if not main_class.children:
             return tree
+
         #add main class to children of classes block
         classes.children.append(main_class)
         return tree
